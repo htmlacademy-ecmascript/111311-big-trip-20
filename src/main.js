ed29dcb179bf7f8d46ce92f327856dcbef25a910
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-29 00:10:01 +0400

    Added rendering of the main element.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import {render} from './render';
+import TripEventsPresenter from './presenter/trip-events-presenter';
 import TripFiltersView from './view/trip-filters-view';
 
 const pageHeader = document.querySelector('.page-header');
 const tripControlsFiltersElement = pageHeader.querySelector('.trip-controls__filters');
 
+const siteMainElement = document.querySelector('.page-main');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
+const boardPresenter = new TripEventsPresenter({container: tripEventsElement});
+
 render(new TripFiltersView(), tripControlsFiltersElement);
+
+boardPresenter.init();
