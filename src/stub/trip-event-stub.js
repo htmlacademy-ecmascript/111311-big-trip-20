b575f8363ee07edc860e2d674893ac196d2953ac
Denis Bogdanov <bogdanov.math@gmail.com>
2023-04-30 09:17:55 +0400

    Added stub for trip event.

---
+++ src/stub/trip-event-stub.js
@@ -0,0 +1,25 @@
+const tripEventStub = {
+  'basePrice': 366,
+  'dateFrom': '2023-06-14T01:28:01.397Z',
+  'dateTo': '2023-06-15T07:28:01.397Z',
+  'destination': {
+    'description': 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    'name': 'Chamonix',
+    'pictures': [
+      {
+        'src': 'http://picsum.photos/300/200?r=0.0762563005163317',
+        'description': 'Chamonix parliament building'
+      }
+    ]
+  },
+  'isFavorite': true,
+  'offers': [
+    {
+      'title': 'Upgrade to a business class',
+      'price': 120
+    }
+  ],
+  'type': 'restaurant'
+};
+
+export {tripEventStub};
